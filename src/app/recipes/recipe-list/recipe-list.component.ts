e549e4e07fa64f5b9218be3cb8f9a1674b2dfd29
Davor Roman <davoroman01@gmail.com>
2022-09-20 14:49:29 +0200

    First commit
    Udemy Project

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+	selector: 'app-recipe-list',
+	templateUrl: './recipe-list.component.html',
+	styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+	/* VARIABLES */
+	recipes: Recipe[] = [
+		new Recipe(
+			'A Test Recipe',
+			'This is test recipe',
+			'https://placeimg.com/100/100/nature'
+		),
+	];
+
+	/* INITIAL METHODS */
+	constructor() {}
+
+	ngOnInit(): void {}
+
+	/* METHODS */
+}
